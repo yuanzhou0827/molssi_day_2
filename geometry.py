15c0d5723af6267114afb4aa8217aabcb1ec1e43
yuanzhou <yuanzhou@umich.edu>
2019-07-23 13:42:19 -0500

    add documentation for build_bond_list function

--- geometry.py
+++ geometry.py
@@ -21,7 +21,23 @@ def calculate_distance_list(rA, rB):
     return distance
 
 
-def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
+def build_bond_list(coordinates, max_bond=2.93, min_bond=0):
+    """Build list of bonds from atomic coordinates based on distance.
+
+    Parameters
+    ----------
+    coordinates : np.array
+        An array of atomic coordiantes. Size should be (n,3) where n is the number of particles.
+    max_bond : float, optional
+        The maximum distance between atoms to be considered a bond. Default is 2.93 bohr.
+    min_bond:
+        The minimum distance between atoms to be considered a bond.
+
+    Returns
+    -------
+    bonds : dict
+        A dictinary of bonds with atom pair tuples as keys, and calculate bond length as values.
+    """
     num_atoms = len(coordinates)
     
     bonds = {}
